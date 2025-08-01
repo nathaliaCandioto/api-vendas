33b06b57ad70697cbcc183c1cf42c44240ce8db4
nathaliacandioto <nathaliacandioto@gmail.com>
2025-08-01 13:02:53 -0300

    atualização/criação de metodos

--- src/main/java/br/com/vendas/api/controller/VendedorController.java
+++ src/main/java/br/com/vendas/api/controller/VendedorController.java
@@ -1,10 +1,24 @@
 package br.com.vendas.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.vendas.api.entity.Vendedor;
+import br.com.vendas.api.repository.VendedorRepository;
+import br.com.vendas.api.service.VendedorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping ("/vendedor")
 public class VendedorController {
+
+    private VendedorService vendedorService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public  void cadastrarVendedor(@RequestBody Vendedor vendedor){
+        vendedorService.cadastrarVendedor(vendedor);
+    }
+
+
+
 
 }
--- src/main/java/br/com/vendas/api/service/VendedorService.java
+++ src/main/java/br/com/vendas/api/service/VendedorService.java
@@ -4,6 +4,8 @@ import br.com.vendas.api.entity.Vendedor;
 import br.com.vendas.api.repository.VendedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class VendedorService {
 
     @Autowired
@@ -11,6 +13,15 @@ public class VendedorService {
 
     public Vendedor cadastrarVendedor(Vendedor vendedor){
         return vendedorRepository.save(vendedor);
+
+    }
+
+    public List<Vendedor>vendedores(){
+        return vendedorRepository.findAll();
+    }
+
+    public List<Vendedor> findByatributes(String cpf, String nome, String email, String telefone,Double salario,Double comissao){
+        return vendedorRepository.findByAtributtes(cpf, nome, email, telefone,salario,comissao);
 
     }
 }

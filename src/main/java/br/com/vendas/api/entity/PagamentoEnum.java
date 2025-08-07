2053b912ebe89f4d3385591faa1efafdb0657591
nathaliacandioto <nathaliacandioto@gmail.com>
2025-08-07 10:00:31 -0300

    refatoracao de codigo atualizacao do construtor

--- src/main/java/br/com/vendas/api/entity/PagamentoEnum.java
+++ src/main/java/br/com/vendas/api/entity/PagamentoEnum.java
@@ -2,6 +2,14 @@ package br.com.vendas.api.entity;
 
 public enum PagamentoEnum {
 
-    CREDITO,DEBITO,PIX,DINHEIRO
+    CREDITO("credito"),DEBITO("debito"),PIX("pix"),DINHEIRO("dinheiro");
+    private String descricao;
+    private PagamentoEnum(String descricao){
+        this.descricao=descricao;
 
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
